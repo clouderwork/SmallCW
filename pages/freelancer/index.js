8bf78b834f4ed4f372cfe9442e3e08311fc56a01
唐岗 <429517385@qq.com>
2017-01-11 15:24:52 +0800

    处理查找服务方

--- pages/freelancer/index.js
+++ pages/freelancer/index.js
@@ -1,10 +1,9 @@
 //index.js
-//获取应用实例
-var app = getApp()
+var ywk = require('../../utils/ywk')
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    users: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,16 +12,13 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo: {
-          nickName: '找人'
-        }
-      })
+    wx.showLoading()
+    ywk.ajaxJson('/api/freelancers/search', {}, 'GET').then((res) => {
+      console.log(res)
+      wx.hideLoading()
+    }, (err) => {
+      console.log(err)
+      wx.hideLoading()
     })
   }
 })
