c2267e02cf1564efc0e7cf18fc03cced5c29d171
唐岗 <429517385@qq.com>
2017-01-11 16:32:19 +0800

    处理服务方

--- pages/freelancer/index.js
+++ pages/freelancer/index.js
@@ -3,7 +3,8 @@ var ywk = require('../../utils/ywk')
 
 Page({
   data: {
-    users: []
+    users: [],
+    windowHeight: 100
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,13 +14,26 @@ Page({
   },
   onLoad: function () {
     wx.showLoading()
+
     ywk.ajaxJson('/api/freelancers/search', {}, 'GET').then((res) => {
-      this.users = res.users
+      if (res && res.data) {
+        this.users = res.data.users
+      }
       console.log(this.users)
       wx.hideLoading()
     }, (err) => {
       console.log(err)
       wx.hideLoading()
     })
+  },
+  onShow () {
+    // 获取页面高度
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          windowHeight: res.windowHeight
+        })
+      }
+    });
   }
 })
