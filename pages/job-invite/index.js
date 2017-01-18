c693a4d79b373d9fb4e640cefa12c00bf4273d41
唐岗 <429517385@qq.com>
2017-01-18 19:42:41 +0800

    XCX-37

--- pages/job-invite/index.js
+++ pages/job-invite/index.js
@@ -4,6 +4,7 @@ var ywk = require('../../utils/ywk')
 Page({
   data: {
     id: '',
+    isDisable: 'disabled',
     type: 'team',
     time: ['请选择', '一周以内', '一个月内', '1-3个月', '3-6个月', '6个月以上'],
     objectTime: [
@@ -46,18 +47,30 @@ Page({
       this.setData({
           index: e.detail.value
       })
+      this.ckDis()
   },
   chooseJob (e) {
     this.setData({
         jobIndex: e.detail.value
+    })
+    this.ckDis()
+  },
+  ckDis () {
+    let dis = 'disabled'
+    if (!(this.data.index === 0 || this.data.jobIndex === 0)) {
+      dis = ''
+    }
+    this.setData({
+      isDisable: dis
     })
   },
   getData () {
     let query = { rtype: 'invite' }
     if (this.data.type === 'team') {
       query.team_id = this.data.id
+    } else {
+      query.user_id = this.data.id
     }
-    console.log('getData')
     ywk.ajaxJson(`/api/jobs/invite`, query).then((res) => {
       let job = this.data.jobs
       let jobObj = this.data.jobObj
@@ -83,7 +96,6 @@ Page({
     }
   },
   onLoad (opt) {
-    console.log(opt)
     if (opt && opt.id) {
       this.setData({
         id: opt.id,
@@ -91,5 +103,39 @@ Page({
       })
       this.getData()
     }
+  },
+  submit () {
+    if (this.data.isDisable === 'disabled') return false;
+    let data = {
+      job_id: this.data.jobObj[this.data.jobIndex].id,
+      duration: this.data.objectTime[this.data.index].id,
+      message: '这里是邀请的文案'
+    }
+    if (this.data.type === 'team') {
+      data.team_id = this.data.id
+    } else {
+      data.user_id = this.data.id
+    }
+    ywk.ajaxJson('/api/proposal', data, 'POST').then((res) => {
+      if (res.error_code === 0) {
+        wx.showToast({
+          title: '提交成功',
+          icon: 'success',
+          duration: 2000
+        })
+
+        setTimeout(function(){
+          wx.navigateTo({
+            url: '../profile/index'
+          })
+        },2000)
+      } else {
+        wx.showToast({
+          title: res.msg,
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    }, (err) => {})
   }
 })
